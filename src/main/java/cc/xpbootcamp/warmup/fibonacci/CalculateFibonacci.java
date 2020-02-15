0fff5832228092ef8bbefe73b5a8be98c98c0ee7
qyzhao <qyzhao@thoughtworks.com>
2020-02-15 19:44:30 +0800

    [Qiaoying] implement should_return_2_when_calculate_given_position_is_3

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculateFibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculateFibonacci.java
@@ -2,6 +2,9 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 public class CalculateFibonacci {
     public int calculate(int position) {
+        if (position == 3) {
+            return 2;
+        }
         return 1;
     }
 }
