b767ff233833c962edc17658c222c4c03c9f48ac
qyzhao <qyzhao@thoughtworks.com>
2020-02-15 19:42:03 +0800

    [Qiaoying] add test should_return_2_when_calculate_given_position_is_3

--- src/test/java/cc/xpbootcamp/warmup/fibonacci/CalculateFibonacciTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/CalculateFibonacciTest.java
@@ -9,12 +9,23 @@ public class CalculateFibonacciTest {
     public void should_return_1_when_calculate_given_position_is_1() {
         //given
         CalculateFibonacci calculateFibonacci = new CalculateFibonacci();
-        int position = 1;
 
         //when
-        int result = calculateFibonacci.calculate(position);
+        int result = calculateFibonacci.calculate(1);
 
         //then
         assertEquals(1, result);
+    }
+
+    @Test
+    public void should_return_2_when_calculate_given_position_is_3() {
+        //given
+        CalculateFibonacci calculateFibonacci = new CalculateFibonacci();
+
+        //when
+        int result = calculateFibonacci.calculate(3);
+
+        //then
+        assertEquals(2, result);
     }
 }
