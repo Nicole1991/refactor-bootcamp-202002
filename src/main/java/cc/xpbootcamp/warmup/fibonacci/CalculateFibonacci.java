d81fe34296325bbc03ca04c062fa535ef67bd986
qyzhao <qyzhao@thoughtworks.com>
2020-02-15 20:01:30 +0800

    [Qiaoying] Fibonacci: refactor: change the implement way to improve efficiency

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculateFibonacci.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculateFibonacci.java
@@ -4,7 +4,14 @@ public class CalculateFibonacci {
     public Long calculate(int position) {
         if (position < 2) {
             return (long) position;
+        } else {
+            Long[] answer = new Long[position];
+            answer[0] = 1L;
+            answer[1] = 1L;
+            for (int i = 2; i < position; i++) {
+                answer[i] = answer[i-1] + answer[i-2];
+            }
+            return answer[position-1];
         }
-        return calculate(position - 1) + calculate(position - 2);
     }
 }
