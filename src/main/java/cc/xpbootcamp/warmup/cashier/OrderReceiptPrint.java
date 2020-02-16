da9f2ead351db3f94f9e9c99a898a242dddb8ce1
qyzhao <qyzhao@thoughtworks.com>
2020-02-16 11:56:36 +0800

    [Qiaoying] Refactor: remove unused annotation

--- src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceiptPrint.java
+++ src/main/java/cc/xpbootcamp/warmup/cashier/OrderReceiptPrint.java
@@ -1,12 +1,5 @@
 package cc.xpbootcamp.warmup.cashier;
 
-/**
- * OrderReceipt prints the details of order including customer name, address, description, quantity,
- * price and amount. It also calculates the sales tax @ 10% and prints as part
- * of order. It computes the total order amount (amount of individual lineItems +
- * total sales tax) and prints it.
- *
- */
 public class OrderReceiptPrint {
     private Order order;
 
@@ -61,8 +54,7 @@ public class OrderReceiptPrint {
     private double calculateTotalPrice() {
         double totalSalesTax = 0d;
         for (LineItem lineItem : order.getLineItems()) {
-            double salesTax = lineItem.totalAmount() * .10;
-            totalSalesTax += salesTax;
+            totalSalesTax += lineItem.totalAmount() * .10;
         }
         return totalSalesTax;
     }
